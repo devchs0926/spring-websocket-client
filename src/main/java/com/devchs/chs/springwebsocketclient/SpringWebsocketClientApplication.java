2cdc0c1bafff261db0f419759acea94e5c1bf948
devchs0926 <devchs0926@gmail.com>
2023-01-15 00:22:31 +0900

    initial commit

---
+++ src/main/java/com/devchs/chs/springwebsocketclient/SpringWebsocketClientApplication.java
@@ -0,0 +1,12 @@
+package com.devchs.chs.springwebsocketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketClientApplication.class, args);
+    }
+}
---
+++ src/test/java/com/devchs/chs/springwebsocketclient/SpringWebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchs.chs.springwebsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
